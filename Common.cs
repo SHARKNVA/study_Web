cbb1ff5da3ce970922fbca0be7109aafe126e21d
SHARKNVA <138999796+SHARKNVA@users.noreply.github.com>
2025-10-20 17:54:42 +0700

    Update Common.cs

--- Common.cs
+++ Common.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Data;
 using System.Data.SqlClient;
@@ -35,30 +35,61 @@ namespace WebApplication1.App_Start
             return new OleDbConnection(Connect_UserSQL);
         }
 
+        //public static DataTable ExcuteDataTable_User(string query)
+        //{
+        //    using (SqlConnection conn = new SqlConnection(Connect_UserSQL))
+        //    {
+        //        using (SqlDataAdapter dap = new SqlDataAdapter(query, conn))
+        //        {
+        //            using (DataSet ds = new DataSet())
+        //            {
+        //                dap.Fill(ds);
+        //                conn.Close();
+        //                conn.Dispose();
+        //                return ds.Tables[0];
+        //            }
+        //        }
+        //    }
+        //}
         public static DataTable ExcuteDataTable_User(string query)
         {
-            using (SqlConnection conn = new SqlConnection(Connect_UserSQL))
+            using (OleDbConnection conn = new OleDbConnection(Connect_UserSQL))
             {
-                using (SqlDataAdapter dap = new SqlDataAdapter(query, conn))
+                using (OleDbDataAdapter dap = new OleDbDataAdapter(query, conn))
                 {
                     using (DataSet ds = new DataSet())
                     {
                         dap.Fill(ds);
-                        conn.Close();
-                        conn.Dispose();
                         return ds.Tables[0];
                     }
                 }
             }
         }
+        //public static void Excute_SQL(string sql)
+        //{
+        //    using (OleDbConnection conn = new OleDbConnection(Connect_UserSQL))
+        //    {
+        //        conn.Open();
+        //        using (OleDbCommand cmd = new OleDbCommand(sql, conn))
+        //        {
+        //            cmd.Connection = GetConnectionUser();
+        //            cmd.ExecuteNonQuery();
+        //        }
+        //    }
+        //}
+
         public static void Excute_SQL(string sql)
         {
-            using (OleDbConnection con = new OleDbConnection(Connect_UserSQL))
+            using (OleDbConnection conn = Common.GetConnectionUser())
             {
-                using (OleDbCommand cmd = new OleDbCommand(sql))
+                conn.Open();
+                using (OleDbCommand cmd = new OleDbCommand(sql, conn))
+                
                 {
-                    cmd.Connection = GetConnectionUser();
-                    cmd.ExecuteNonQuery();
+                   cmd.CommandText = sql;
+                        cmd.Prepare();
+                        cmd.ExecuteNonQuery();
+                        
                 }
             }
         }
